8860b68406bfdbadc208bd55867cc48b4cf510dd
lincey-cherian-au7 <linceycherian@gmail.com>
2020-11-19 00:14:37 +0530

    first commit

---
+++ client/src/Components/SignUp.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './SignUp.css'
+
+class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container ">
+                    <div className="wrap-container">
+                   
+                    <form className="form">
+                    <fieldset>
+                    <legend>User Registration Form</legend> 
+                        <div className="row">
+                            <div class="column">
+                                <label >First Name</label>
+                                <input type="text" name="fname" placeholder="Firstname"></input>
+                            </div>
+                           <div class="column">
+                                <label>Last Name</label>
+                                <input type="text" name="lname" placeholder="Lastname"></input>
+                           </div>
+                        </div>
+                        <br/>
+                        <div className="row">
+                            <div class="column">
+                                <label >Email</label>
+                                <input type="text" name="Email" placeholder="Email"></input> 
+                            </div>
+                        </div><br/>
+                        <div className="row">
+                            <div class="column">
+                                <label>Password</label>
+                                <input type="password" name="Password" placeholder="Password"></input>
+                            </div>
+                            <div class="column">
+                                <label>Confirm Password</label>
+                                <input type="password" name="ConfirmPassword" placeholder="ConfirmPassword"></input>
+                            </div>
+                        </div><br/>
+                        <div>
+                            <button type="submit" className="button">Register</button>
+                        </div>
+                        </fieldset>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
