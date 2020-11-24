e5b3c44732051b29cf2af6de118708ad1dffae74
lincey-cherian-au7 <linceycherian@gmail.com>
2020-11-24 10:19:40 +0530

    Home Page design completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 //import SignUp from './Components/SignUp/SignUp';
-import SignIn from './Components/SignIn/SignIn'
+// import SignIn from './Components/SignIn/SignIn'
 import Navigation from './Components/Navigation/Navigation'
-import LoanApplication from './Components/LoanApplication/LoanApplication';
+// import LoanApplication from './Components/LoanApplication/LoanApplication';
 //import MainPage from './Components/MainPage/MainPage';
+import HomePage from  './Components/HomePage/HomePage'
+
 
 
 function App() {
@@ -13,7 +15,8 @@ function App() {
       <Navigation/>
       {/* <SignIn/> */}
       {/* <MainPage/ */}
-      <LoanApplication/>
+      {/* <LoanApplication/> */}
+      <HomePage/>
     </div>
   );
 }
